79a0bb966b9341ba61dca2992761860be76593b7
noytau <noyhassid@mail.tau.ac.il>
2025-07-23 15:48:59 +0300

    Adding custom feature extractor & training only it, computes loss for student & teacher

--- customize_model.py
+++ customize_model.py
@@ -3,7 +3,9 @@ import torch.nn as nn
 class CustomFeatureExtractor(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv = nn.Conv1d(1, 1, kernel_size=5, stride=5)  # 245 → 49
+        #self.conv = nn.Conv1d(1, 1, kernel_size=5, stride=5)  # 245 → 49
+        self.conv = nn.Conv1d(1, 512, kernel_size=5, stride=5)
 
     def forward(self, x):  # x: [B, 1, 245]
-        return self.conv(x)  # → [B, 1, 49]
+        x = self.conv(x)
+        return x
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 # internal imports
 from data_parser import run_data_parser, summarize_data_overview, convert_to_huggingface_dataset
 from model_loader import prepare_resampled_dataloader, resample_to_16k, mask_spectrogram, plot_masked_dataset_statistics, load_data2vec_audio_model, run_model_on_dataset, train_self_supervised, evaluate_embeddings
+from model_loader import *
+from customize_model import *
 from compute_stats import compute_cosine_similarity_matrix
 from datasets import Dataset
 import pandas as pd
@@ -19,9 +21,10 @@ if __name__ == '__main__':
     single_chnl_df = (run_data_parser()) # returns
     # summarize_data_overview(single_chnl_df) enable to create a summary of the dataset
 
-    model, feature_extractor, device = load_data2vec_audio_model()
-    dataset = prepare_resampled_dataloader(single_chnl_df)
-    compute_cosine_similarity_matrix(dataset)  # Compute and plot cosine similarity matrix
+    model, feature_extractor, optimizer, device = load_data2vec_audio_model()
+    dataset = prepare_masked_dataloader(single_chnl_df, interpolate_to_16k=False)
+    # compute_cosine_similarity_matrix(dataset)  # Compute and plot cosine similarity matrix
     # plot_masked_dataset_statistics(masked_dataset, output_dir="plots/single_channel")
-
-    features = run_model_on_dataset(dataset, model, model.feature_extractor, device)
+    # train only the feature extractor
+    train_feature_extractor_only(model, optimizer, dataset, device)
+    #features = run_model_on_dataset(dataset, model, model.feature_extractor, device)
