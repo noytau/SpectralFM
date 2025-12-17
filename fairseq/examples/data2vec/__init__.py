0a152496d932ff4baefece44f8c4a7366ec1a410
noytau <noyhassid@mail.tau.ac.il>
2025-12-17 17:31:03 +0200

    fix

--- fairseq/examples/data2vec/__init__.py
+++ fairseq/examples/data2vec/__init__.py
@@ -3,5 +3,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import data2vec.data
-import data2vec.criterions
+from . import data
+from . import criterions
