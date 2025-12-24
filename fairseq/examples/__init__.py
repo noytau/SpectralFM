9e245e04b48adad85d3ba4be0f241bde9e031135
root <noyhassid@mail.tau.ac.il>
2025-12-24 17:42:59 +0000

    Updating import path + conda file

--- fairseq/examples/__init__.py
+++ fairseq/examples/__init__.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .data2vec.models import *
 try:
     from fairseq.version import __version__  # noqa
 except ImportError:
